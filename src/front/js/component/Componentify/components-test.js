4493abb5bc34438bf994c8c0588064b255e5e1a5
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-09-13 17:39:34 +0000

    Testing

---
+++ src/front/js/component/Componentify/components-test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Chau = () => {
+    return (
+        <div>Holis</div>
+    )
+}
+
+
+
+export const Component = () => {
+    return (
+        <div>chau</div>
+    )
+}
+
+
