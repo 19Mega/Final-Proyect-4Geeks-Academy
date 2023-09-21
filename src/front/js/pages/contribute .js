99bc4b7302ea1ddd1d59548c93646bd7a0f11da8
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-09-21 14:37:46 +0000

    actualizar card

--- src/front/js/pages/contribute .js
+++ src/front/js/pages/contribute .js
@@ -12,28 +12,52 @@ const Contribute = () => {
     window.location.replace(direccion)
   }
   return (
-    <div className="container" id="plans" style={{ marginBottom: '150px', marginTop: '150px' }}>
+    <div className="container" id="plans" style={{ marginBottom: '150px', marginTop: '50px', textAlign: 'center' }}>
       <h1 className="my-4" style={{ fontSize: '3rem' }}>
         <strong> Contribute <span style={{ "color": "#FD5812" }}> always</span></strong>
       </h1>
-      <h4>At Componentify, we're dedicated to making a difference. Through our innovative solutions and socially responsible initiatives, we're actively shaping a brighter future. Together, let's contribute to a better tomorrow.
+      <h5>At Componentify, we're dedicated to making a difference. Through our innovative solutions and socially responsible initiatives, we're actively shaping a brighter future. Together, let's contribute to a better tomorrow.
+      </h5>
+      <h5>
+        We're thrilled to offer you these high-quality components for free. However, maintaining and improving this platform takes time and resources.
+      </h5>
+      <h5>
+        If you appreciate what we do and want to support our work, consider making a donation. Every small contribution helps us continue creating useful and functional tools for you.
+      </h5>
+      <h5>
+        We greatly appreciate your support and trust in us!
 
-</h4>
+        Thank you for being a part of the Componentify community!
+      </h5>
+      <div style={{ background: "linear-gradient(to right, #900C3F, #FD5812)" }}>
+        <div className="container">
+          <hr style={{ border: "1px solid #FFF" }} />
+        </div>
+      </div>
+      <h5>
+        **Ways to Donate:**
+      </h5>
+
       <div className="row">
         <div className="col-md-4">
-          <div className="card shadow" style={{ backgroundColor: '#FFDAB9' }}>
-            <div className="card-header">
-              <h5></h5>
-            </div>
+          <div className="card shadow" style={{ backgroundColor: '#FFDAB9',height:"300px" }}>
+             <div className="card-header">
+              <h5>Support Our Work!</h5>
+                
+            </div> 
             <div className="card-body">
+            <p>
+            Make a one-time payment or a recurring contribution.</p>
+                 <hr/>
               <h6 className="card-subtitle mb-2 text-muted">$10.00/month</h6>
-              <p></p>
-              <button className="btn btn-success" 
-              onClick={pagar}
+              
+              <button className="btn btn-success"
+                onClick={pagar}
               >Contribute</button>
             </div>
           </div>
         </div>
+        {/* <div className='card card-large shadow' style={el estilo q tiene}> */}
         <div className="col-md-4">
           <div className="card shadow" style={{ backgroundColor: '#FFB93E' }}>
             <div className="card-header">
@@ -41,29 +65,17 @@ const Contribute = () => {
             </div>
             <div className="card-body">
               <h6 className="card-subtitle mb-2 text-muted">$50.00/month</h6>
-              <p>
 
-We're thrilled to offer you these high-quality components for free. However, maintaining and improving this platform takes time and resources. If you appreciate what we do and want to support our work, consider making a donation. Every small contribution helps us continue creating useful and functional tools for you.
-
-**Ways to Donate:**
-
-- [PayPal](your-PayPal-link)
-- [Bitcoin](your-Bitcoin-address)
-
-We greatly appreciate your support and trust in us!
-
-Thank you for being a part of the Componentify community!
-</p>
               <button className="btn btn-success">Contribute</button>
             </div>
           </div>
         </div>
         <div className="col-md-4 height: 500px;">
-       
+
           <div className="card shadow" style={{ backgroundColor: '#FFA55E' }}>
-            <div className="card-header"> 
-            
-            
+            <div className="card-header">
+
+
               <h5></h5>
               {/* <p>Descripción del Pro Componentify...</p> */}
             </div>
