f2b9ba30110efa3510e61b719bafa37406085e9f
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-09-21 15:09:43 +0000

    How to integrate componentify

--- src/front/js/pages/alertView.js
+++ src/front/js/pages/alertView.js
@@ -8,9 +8,6 @@ import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 
 const AlertView = () => {
-
-
-    const myStyle = { padding: "1rem 1rem" }
     return (
 
 
@@ -220,3 +217,27 @@ const AlertView = () => {
 
 
 export default AlertView;
+// ESTRUCTURA
+// <div className="container">
+//     <div className="row">
+//     <SideBar />
+//         <div className="col-10">
+//             <div className="container">
+//                 <div className="row">
+//                     <h1 className="mt-3">Título del componente</h1>
+//                     <p className="fs-5 mt-2 mb-3">Párrafo explicando el componente.</p>
+//                 </div>
+//                 <div className="row">
+//                     <div className="col-lg-3 col-md-3 col-sm-3 col-12">
+//                         Acá va el componente
+//                     </div>
+//                     <div className="col-lg-9 col-md-9 col-sm-9 col-12">
+//                         <SyntaxHighlighter className="mt-0 c-alert" language="html" style={prism}>
+//                             {`Acá va el código`}
+//                         </SyntaxHighlighter>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
