577a9e31270f0a0442a7cc99ba0f4706c029c330
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-09-24 19:08:31 +0000

    New login and global css

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import ComponentifyLogoOrange from "../../img/componentify-logo-orange.png";
 
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -54,75 +55,151 @@ const Login = () => {
     });
 
 
-      const menuColor = "#900C3F"
+      const menuColor = "#FD5812"
 
       return (
 
-        <div style={{background: "linear-gradient(to right, rgb(144 12 63 / 5%), rgb(144 12 63 / 5%))"}}>
-<div className="container">
-    <div className="row">
-      <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-      <div className="card my-5" style={{ borderImage: 'linear-gradient(to right, #900C3F, #FD5812)', borderImageSlice: '1', borderStyle: 'solid', borderWidth: '6px'}}>
-          <div className="card-body p-2 p-sm-5" style={{backgroundColor:"#c7b08c1f"}}>
-            <h5 className="card-title text-center mb-5 fw-light fs-2 fw-semibold" style={{color:"#900C3F"}}>Log In</h5>
-            <form onSubmit={formik.handleSubmit} >
+        <section class="vh-100">
+          <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+              <div class="col col-xl-10">
+                <div class="card" style={{ borderRadius: "1rem", boxShadow: "0 2px 45px #FF5733" }}>
+                  <div class="row g-0">
+                    <div class="col-md-6 col-lg-5 d-none d-md-block">
+                      <img src="https://images.pexels.com/photos/5122177/pexels-photo-5122177.jpeg?auto=compress&cs=tinysrgb&w=600"
+                        alt="login form" class="img-fluid" style={{ borderRadius: "1rem 0 0 1rem" }} />
+                    </div>
+                    <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                      <div class="card-body p-4 p-lg-5 text-black">
 
-              <div className="form-floating mb-4">
-                <input className="form-control" placeholder="name@example.com"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-                id="email"
-                name="email"
-                type="email"
-                />
+                        <form onSubmit={formik.handleSubmit}>
 
-                {formik.touched.email && formik.errors.email ? (
-                    <div style={{ color: menuColor}} >{formik.errors.email}</div>    
-                ) : null}
+                          <div class="d-flex align-items-center mb-3 pb-1">
+                            <img width="60"
+                              height="54" src={ComponentifyLogoOrange} />
+                            <span class="h1 fw-bold mb-0">Componentify</span>
+                          </div>
+
+                          <h5 class="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Log into your account</h5>
+
+                          <div class="form-outline mb-4">
+                            <input class="form-control form-control-lg" placeholder="name@example.com"
+                              onChange={formik.handleChange}
+                              value={formik.values.email}
+                              id="email"
+                              name="email"
+                              type="email"
+                            />
+                            {formik.touched.email && formik.errors.email ? (
+                              <div style={{ color: menuColor }} >{formik.errors.email}</div>
+                            ) : null}
+                            <label class="form-label" for="form2Example17">Email address</label>
+                          </div>
+
+                          <div class="form-outline mb-4">
+                            <input className="form-control form-control-lg"
+                              placeholder="Password"
+                              onChange={formik.handleChange}
+                              value={formik.values.password}
+                              id="password"
+                              name="password"
+                              type="password" />
+                            {formik.touched.password && formik.errors.password ? (
+                              <div style={{ color: menuColor }} >{formik.errors.password}</div>
+                            ) : null}
+                            <label class="form-label" for="form2Example27">Password</label>
+                          </div>
+
+                          <div class="pt-1 mb-4">
+                            <button class="c-btn c-btn-componentify c-btn-componentify-hover btn-lg btn-block" type="submit">Login</button>
+                          </div>
+
+                          <a class="small text-fog" href="#!">Forgot password?</a>
+                          <p class="mb-5 pb-lg-2 text-dark">Don't have an account yet? <Link to="/signup" className="text-componentify">Register here</Link>
+                          </p>
+                        </form>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+      );
+}
+ export default Login;
+
+//         <div style={{background: "linear-gradient(to right, rgb(144 12 63 / 5%), rgb(144 12 63 / 5%))"}}>
+// <div className="container">
+//     <div className="row">
+//       <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+//       <div className="card my-5" style={{ borderImage: 'linear-gradient(to right, #900C3F, #FD5812)', borderImageSlice: '1', borderStyle: 'solid', borderWidth: '6px'}}>
+//           <div className="card-body p-2 p-sm-5" style={{backgroundColor:"#c7b08c1f"}}>
+//             <h5 className="card-title text-center mb-5 fw-light fs-2 fw-semibold" style={{color:"#900C3F"}}>Log In</h5>
+//             <form onSubmit={formik.handleSubmit} >
+
+//               <div className="form-floating mb-4">
+//                 <input className="form-control" placeholder="name@example.com"
+//                 onChange={formik.handleChange}
+//                 value={formik.values.email}
+//                 id="email"
+//                 name="email"
+//                 type="email"
+//                 />
+
+//                 {formik.touched.email && formik.errors.email ? (
+//                     <div style={{ color: menuColor}} >{formik.errors.email}</div>    
+//                 ) : null}
                 
-                <label for="floatingInput" style={{ color: menuColor}}>Email address</label>
-              </div>
+//                 <label for="floatingInput" style={{ color: menuColor}}>Email address</label>
+//               </div>
 
-              <div className="form-floating mb-4">
-                <input className="form-control" placeholder="Password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                id="password"
-                name="password"
-                type="password"
-                />
+//               <div className="form-floating mb-4">
+//                 <input className="form-control" placeholder="Password"
+//                 onChange={formik.handleChange}
+//                 value={formik.values.password}
+//                 id="password"
+//                 name="password"
+//                 type="password"
+//                 />
                 
-                {formik.touched.password && formik.errors.password ? (
-                    <div style={{ color: menuColor}} >{formik.errors.password}</div>    
-                ) : null}
+//                 {formik.touched.password && formik.errors.password ? (
+//                     <div style={{ color: menuColor}} >{formik.errors.password}</div>    
+//                 ) : null}
 
-                <label for="floatingPassword" style={{ color: menuColor}}>Password</label>
-              </div>
+//                 <label for="floatingPassword" style={{ color: menuColor}}>Password</label>
+//               </div>
 
-              <div className="form-check mb-3">
-                <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
-                <label className="form-check-label" htmlFor="rememberPasswordCheck" style={{ color: menuColor}}>
-                  Remember password
-                </label>
-              </div>
-              <div className="d-grid">
-                <button className="btn btn-login text-uppercase fw-bold text-white" type="submit" style={{backgroundColor:menuColor}}>Log in</button>
-              </div>
-              <hr className="my-4"/>
+//               <div className="form-check mb-3">
+//                 <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
+//                 <label className="form-check-label" htmlFor="rememberPasswordCheck" style={{ color: menuColor}}>
+//                   Remember password
+//                 </label>
+//               </div>
+//               <div className="d-grid">
+//                 <button className="btn btn-login text-uppercase fw-bold text-white" type="submit" style={{backgroundColor:menuColor}}>Log in</button>
+//               </div>
+//               <hr className="my-4"/>
 
-              <div className="d-grid mb-2">
-                <button className="btn btn-google btn-login text-uppercase text-white fw-bold border border-2" type="submit" style={{backgroundColor:"#3c434c"}}>
-                <i className="fa-brands fa-github fa-lg me-2"></i> Sign in with Github
-                </button>
-              </div>
+//               <div className="d-grid mb-2">
+//                 <button className="btn btn-google btn-login text-uppercase text-white fw-bold border border-2" type="submit" style={{backgroundColor:"#3c434c"}}>
+//                 <i className="fa-brands fa-github fa-lg me-2"></i> Sign in with Github
+//                 </button>
+//               </div>
               
-              <div className="d-grid">
-                <Link to="/signup" className="btn btn-login text-uppercase fw-bold text-white" type="submit" style={{backgroundColor:menuColor}}>Sing up</Link>
+//               <div className="d-grid">
+//                 <Link to="/signup" className="btn btn-login text-uppercase fw-bold text-white" type="submit" style={{backgroundColor:menuColor}}>Sing up</Link>
         
-                {/* <button className="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
+                /* <button className="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
                   <i className="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                </button> */}
-              </div>
+                </button> */
+              /* </div>
             </form>
           </div>
         </div>
@@ -130,64 +207,8 @@ const Login = () => {
     </div>
   </div>
       
-  </div>
-
-      );
-}
- export default Login;
+  </div> */
 
 
 
 
-
-
-{/* 
-<div className="container">
-            <h1 className="mt-5 text-center" id="login-title">Login</h1>
-            <div className="d-flex justify-content-center" id="form_container">
-                <form 
-                onSubmit={formik.handleSubmit}
-                id="login-form">
-                    <div className="mb-3">
-                        <label for="email" className="form-label">Email</label>
-                        <input 
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        id="email"
-                        name="email"
-                        type="email"
-                        />
-
-                        {formik.touched.email && formik.errors.email ? (
-                            <div>{formik.errors.email}</div>    
-                        ) : null}
-
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-
-                    <div className="mb-3">
-                        <label for="password" className="form-label">Password</label>
-                        <input 
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        id="password"
-                        name="password"
-                        type="password"
-                        />
-                        
-                        {formik.touched.password && formik.errors.password ? (
-                            <div>{formik.errors.password}</div>    
-                        ) : null}
-
-                        <div id="emailHelp" className="form-text"><Link to={"/none"}>Forgot your password?</Link></div>
-                    </div>
-
-                    <button type="submit" className="btn btn-success" id="btn-login">Login</button>
-                    <div id="emailHelp" className="form-text">Don't have an account yet? <Link to={"/signup"}>Sign Up.</Link></div>
-                </form>
-            </div>
-        </div> */}
-
-
-
- 
