5baedbc3bdfeda5c59e6d85d86c0a90083a1a8f0
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-09-27 03:30:51 +0000

    update badgeview and badge layout

--- src/front/js/pages/badgesView.js
+++ src/front/js/pages/badgesView.js
@@ -1,103 +1,99 @@
 import React, { useState } from 'react';
-import "../../styles/global.css"; 
+import "../../styles/global.css";
 import "./global.js";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { SideBar } from '../component/sideBar';
-import { FaCopy, FaCheck } from 'react-icons/fa';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export const BadgesView = () => {
-  // define el estado local para rastrear si el codigo se ha copiado
-  const [copied, setCopied] = useState({});
+  const [copiedIndexes, setCopiedIndexes] = useState([]);
 
-  // función para copiar el código al portapapeles
-  const copyCode = async (codeKey) => {
-    const code = codes[codeKey]; // agarra el codigo correspondiente a la codekey
+  const handleCopy = (i) => {
+    setCopiedIndexes([...copiedIndexes, i]);
 
-    try {
- // va intentar copiar el código al portapapeles utilizando la  api navigator-clipboard
-      await navigator.clipboard.writeText(code);
- // se va actulizar para marcar que el codigo se ha copiado 
-      setCopied({...copied, [codeKey]: true});
+    setTimeout(() => {
+      setCopiedIndexes((prevIndexes) => prevIndexes.filter((index) => index !== i));
+      console.log('Texto revertido después de 3 segundos');
+    }, 3000); // 3 seconds delay
 
-// temporizador para desmarcar después de 3 segundos
-      setTimeout(() => {
-        setCopied({...copied, [codeKey]: false});
-      }, 3000);
-    } catch (err) {
-      console.error('Error copying to clipboard:', err);
-    }
   };
 
-  // definimos los codigos que se mostrarán 
-
-  const codes = [
-  '<p>Example Text <span class="c-badge text-risk bg-risk">New</span></p>',
-  '<p>Example Text <span class="c-badge text-caution bg-caution">New</span></p>',
-  '<p>Example Text <span class="c-badge text-caution bg-caution">New</span></p>',
-  ];
-
+  const badgeList = [
+    '<p>Example Text <span class="c-badge text-risk bg-risk">New</span></p>',
+    '<p>Example Text <span class="c-badge text-caution bg-caution">New</span></p>',
+    '<p>Example Text <span class="c-badge text-silver bg-silver">New</span></p>',
+    '<p>Example Text <span class="c-badge text-gold bg-gold">New</span></p>',
+    '<p>Example Text <span class="c-badge text-lime bg-lime">New</span></p>',
+    '<p>Example Text <span class="c-badge text-azure bg-azure">New</span></p>',
+    '<p>Example Text <span class="c-badge text-mocca bg-mocca">New</span></p>',
+    '<p>Example Text <span class="c-badge text-aqua bg-aqua">New</span></p>',
+    '<p>Example Text <span class="c-badge text-slate bg-slate">New</span></p>'
+  ]
 
   return (
     <div className="container">
-      <div className="row">
-        <SideBar />
-        <div className="col-10">
-          <div className="container">
-            <div className="row">
-              <h1 className="mt-2 mb-2"><strong>badges</strong></h1> 
+        <div className="row">
+            <SideBar />
+            <div className="col-10">
+
+                  <div className="container">
+
+                      <div className="row">
+                          <h1 className="mt-3">Título del componente</h1>
+                          <p className="fs-5 mt-2 mb-3">Párrafo explicando el componente.</p>
+                      </div>
+
+                                        {/* MAP >>> ACA HACE UN ROW POR CADA COMPONENTE CON SUS COSITAS*/}
+                                        {badgeList.map((item, i) => (
+                                        <div key={i} className='row'>
+
+                                            {/* component */}
+                                            <div className='col-3'>
+                                                {/* Utiliza dangerouslySetInnerHTML para mostrar el HTML */}
+                                                <div dangerouslySetInnerHTML={{ __html: item }} />
+                                            </div>
+
+                                            {/* code */}
+                                            <div className='col-8'>
+                                                <div>
+                                                    <SyntaxHighlighter className="mt-0 c-alert" language="html" style={prism}>
+                                                    {item}
+                                                    </SyntaxHighlighter>
+                                                </div>
+                                            </div>
+
+                                            {/* copy to clipboard */}
+                                            <div className='col-1'>
+                                                <CopyToClipboard text={item} onCopy={() => handleCopy(i)}>
+                                                {copiedIndexes.includes(i) ? <i class="fa-solid fa-check"></i>: <i class="fa-regular fa-clipboard"></i>}
+                                                </CopyToClipboard>
+                                            </div>
+
+                                        </div>
+
+                                        ))}
+
+
+                      {/* proximo subtitulo */}
+                      <div className="row">
+                          <h1 className="mt-3">Título del componente</h1>
+                          <p className="fs-5 mt-2 mb-3">Párrafo explicando el componente.</p>
+                      </div>
+
+                                              
+
+
+
+
+
+
+
+
+                  </div>
             </div>
-            
-            
-         {/* mapeamos los codigos y  se crea una sección de codigo para cada uno */}
-
-            {Object.keys(codes).map(codeKey => (
-              <CodeSection 
-                key={codeKey} 
-                code={codes[codeKey]}
-                copied={copied[codeKey]}
-                style={codeKey}
-                onCopy={() => copyCode(codeKey)}
-              />
-            ))}
-          </div>
-        </div>  
-      </div>
+        </div>
     </div>
   );
 };
-
-const CodeSection = ({code, copied, onCopy, style}) => {
-
-  return (
-    <div className="row">
-
-
-      {/* crea un botón con un estilo específico basado en el valor de 'style' */}
-
-      <div className="col-lg-2 col-md-3 col-sm-3 col-12">
-      {/* <span className={`c-badge text-${color} bg-${color}`}>
-          {style.charAt(0).toUpperCase() + style.slice(1)}
-        </span>  */}
-      </div>
-
-      {/* se muestra el código con resaltado de sintaxis */}
-
-      <div className="col-lg-8 col-md-8 col-sm-8 col-12">
-        <SyntaxHighlighter className="mt-0 c-alert" language="html" style={prism}>
-          {code}
-        </SyntaxHighlighter>
-      </div>
-
-      {/* se crea un boton de copia que muestra un ícono de copia o un ícono de verificación */}
-
-      <div className="col-lg-2 col-md-2 col-sm-2 col-12">
-        <button type="button" className={`c-btn c-btn-silver`} onClick={onCopy}>
-          {copied ? <FaCheck /> : <FaCopy />}
-        </button>
-      </div>
-    </div>
-  );
-};
-
 
